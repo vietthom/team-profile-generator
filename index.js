7bff44e6ef64ad74ce4aae25b8ff827c8e240d58
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-04-20 12:33:58 -0600

    inquirer prompt working and able to generate team profile

--- index.js
+++ index.js
@@ -182,4 +182,25 @@ const addEmployee = ()=>{
     })
 };
 
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err =>{
+        if (err){
+            console.log(err);
+            return;
+        }else {
+            console.log("Your team profile hase been created! Please check out the index.html");
+        }
+    })
+};
 
+addManager()
+    .then(addEmployee)
+    .then(teamArray =>{
+        return generateHTML(teamArray);
+    })
+    .then(pageHTML=>{
+        return writeFile(pageHTML); 
+    })
+    .catch(err=>{
+        console.log(err);
+    });
