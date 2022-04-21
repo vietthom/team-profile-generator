93bda8d4ab85d7aa023f100ff3b6569695dace92
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-04-20 18:54:19 -0600

    test for developer model

---
+++ tests/Developer.test.js
@@ -0,0 +1,19 @@
+const Developer = require('../Models/Developer');
+
+test('create a Developer object', ()=> {
+    const developer = new Developer('Thom', 01, 'thom@gmail.com', 'vietthom');
+
+    expect(developer.github).toEqual(expect.any(String));
+});
+
+test('return github', ()=> {
+    const developer = new Developer('Thom', 01, 'thom@gmail.com', 'vietthom');
+
+    expect(developer.getGithub()).toEqual(expect.stringContaining(developer.github.toString()));
+});
+
+test('return role of employee', ()=> {
+    const developer = new Developer ('Thom', 01, 'thom@gmail.com', 'vietthom');
+
+    expect(developer.getRole()).toEqual('Developer');
+});
